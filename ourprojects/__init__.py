5bf48e34a6e1770a7e02a267526976288edb209a
Alex Wolf <f.alexander.wolf@gmail.com>
2025-01-02 01:39:05 +0100

    ♻️ Prettify

--- ourprojects/__init__.py
+++ ourprojects/__init__.py
@@ -31,7 +31,5 @@ def __getattr__(name):
 
 
 if _check_instance_setup():
-    import lamindb
-
     del __getattr__  # delete so that imports work out
     from .models import Person, Project, Reference
