d1e30615cc13ef2ea24768931e20a990f541eeeb
Alex Wolf <f.alexander.wolf@gmail.com>
2025-01-18 21:53:41 +0800

    Release 1.0.0

--- ourprojects/__init__.py
+++ ourprojects/__init__.py
@@ -19,7 +19,7 @@ The `Reference` registry:
     Reference
 """
 
-__version__ = "1.0a1"  # denote a pre-release for 0.1.0 with 0.1rc1
+__version__ = "1.0.0"  # denote a pre-release for 0.1.0 with 0.1rc1
 
 from lamindb_setup import _check_instance_setup
 
